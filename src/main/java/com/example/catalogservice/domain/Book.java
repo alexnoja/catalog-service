6108cc3892f867fa4d66f0207d324f045e21655e
alexnoja <nojaalex@gmail.com>
2021-06-05 15:19:03 +0300

    Generate constructor using fields, getters and setters instead lombok library.

--- src/main/java/com/example/catalogservice/domain/Book.java
+++ src/main/java/com/example/catalogservice/domain/Book.java
@@ -2,11 +2,6 @@ package com.example.catalogservice.domain;
 
 import java.time.Year;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class Book {
 	
 	private String isbn;
@@ -14,4 +9,53 @@ public class Book {
 	private String author;
 	private Year publishingYear;
 	private Double price;
+
+	public Book(String isbn, String title, String author, Year publishingYear, Double price) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publishingYear = publishingYear;
+		this.price = price;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Year getPublishingYear() {
+		return publishingYear;
+	}
+
+	public void setPublishingYear(Year publishingYear) {
+		this.publishingYear = publishingYear;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 }
